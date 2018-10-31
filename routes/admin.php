e89d587ada7d5bcc0da6e4fa476183c8d44f4332
vagrant <vagrant@homestead>
2018-10-31 12:12:22 +0000

    Managed to get redirect from /homepages to /homepage working

--- routes/admin.php
+++ routes/admin.php
@@ -7,3 +7,8 @@ Route::module('projecttags');
 
 Route::module('homepages');
 Route::name('homepage')->get('homepage', 'HomepageController@homepage');
+
+//redirect /homepages to /homepage in order to disallow people from editing/creating new homepages. "In the end, there can be only one."
+Route::get('homepages', function() {
+    return redirect('homepage');
+});
