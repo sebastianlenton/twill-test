c3396b267fbd3e8e5b22303b5048ac783eb2e29c
vagrant <vagrant@homestead>
2018-11-07 18:09:40 +0000

    removed a trash folder

--- the bin/projecttag.blade.php
+++
@@ -1,9 +0,0 @@
-{{-- this was in the admin blocks dir but it now prevents php artisan twill:blocks from running for some reason}}
-
-@formField('browser', [
-    //'routePrefix' => 'content',
-    'moduleName' => 'projecttags',
-    'name' => 'projecttags',
-    'label' => 'Project Tags',
-    'max' => 10
-])
