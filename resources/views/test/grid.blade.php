8b8a8b6bacf613c6f8939c7e1a08102fef9d3f38
Sebastian Lenton <sebastian.lenton@gmail.com>
2018-11-20 19:08:29 +0000

    Grid

--- resources/views/test/grid.blade.php
+++ resources/views/test/grid.blade.php
@@ -112,26 +112,34 @@
 
     @endfor
 
+    <div class="g g2-12">
+        <p>Grid 2</p>
+    </div>
+    <div class="cf"></div>
+
+    <div class="g g2-12 gp2">
+        <p>Grid 2 gp2</p>
+    </div>
+    <div class="cf"></div>
+
+    <div class="g g2-12 gp4">
+        <p>Grid 2 gp4</p>
+    </div>
+    <div class="cf"></div>
+
+    <div class="g g2-12 gp6">
+        <p>Grid 2 gp6</p>
+    </div>
+    <div class="cf"></div>
+
+    <div class="g g2-12 gp8">
+        <p>Grid 2 gp8</p>
+    </div>
+    <div class="cf"></div>
+
+    <div class="g g2-12 gp10">
+        <p>Grid 2 gp10</p>
+    </div>
+    <div class="cf"></div>
+
 </div>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br><br>
-<br>
-<br>
-<br>
-<br>
-<br>
