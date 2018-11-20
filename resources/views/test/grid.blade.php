0c046944bce2947482943782cd0802bc2fc7b5f4
Sebastian Lenton <sebastian.lenton@gmail.com>
2018-11-20 18:51:02 +0000

    Desktop grid

--- resources/views/test/grid.blade.php
+++ resources/views/test/grid.blade.php
@@ -1,12 +1,137 @@
 <div class="gc">
 
+    <div class="g g1-12">
+        <p>Grid 1</p>
+    </div>
+    <div class="g g1-12">
+        <p>Grid 1</p>
+    </div>
+    <div class="g g1-12">
+        <p>Grid 1</p>
+    </div>
+    <div class="g g1-12">
+        <p>Grid 1</p>
+    </div>
+    <div class="g g1-12">
+        <p>Grid 1</p>
+    </div>
+    <div class="g g1-12">
+        <p>Grid 1</p>
+    </div>
+    <div class="g g1-12">
+        <p>Grid 1</p>
+    </div>
+    <div class="g g1-12">
+        <p>Grid 1</p>
+    </div>
+    <div class="g g1-12">
+        <p>Grid 1</p>
+    </div>
+    <div class="g g1-12">
+        <p>Grid 1</p>
+    </div>
+    <div class="g g1-12">
+        <p>Grid 1</p>
+    </div>
+    <div class="g g1-12">
+        <p>Grid 1</p>
+    </div>
+
+    <div class="cf"></div>
+
     @for ($i = 1; $i <= 12; $i++)
         
         <div class="g g{{ $i }}-12">
-            Grid {{ $i }}
+            <p>Grid {{ $i }}</p>
+        </div>
+        <div class="cf"></div>
+
+    @endfor
+
+    <div class="g g2-12">
+        <p>Grid 2</p>
+    </div>
+    <div class="g g2-12">
+        <p>Grid 2</p>
+    </div>
+    <div class="g g2-12">
+        <p>Grid 2</p>
+    </div>
+    <div class="g g2-12">
+        <p>Grid 2</p>
+    </div>
+    <div class="g g2-12">
+        <p>Grid 2</p>
+    </div>
+    <div class="g g2-12">
+        <p>Grid 2</p>
+    </div>
+    
+    <div class="g g3-12">
+        <p>Grid 3</p>
+    </div>
+    <div class="g g3-12">
+        <p>Grid 3</p>
+    </div>
+    <div class="g g3-12">
+        <p>Grid 3</p>
+    </div>
+    <div class="g g3-12">
+        <p>Grid 3</p>
+    </div>
+
+    <div class="g g4-12">
+        <p>Grid 4</p>
+    </div>
+    <div class="g g4-12">
+        <p>Grid 4</p>
+    </div>
+    <div class="g g4-12">
+        <p>Grid 4</p>
+    </div>
+
+    <div class="g g6-12">
+        <p>Grid 6</p>
+    </div>
+    <div class="g g6-12">
+        <p>Grid 6</p>
+    </div>
+
+    <div class="g g12-12">
+        <p>Grid 12</p>
+    </div>
+
+    <div class="cf"></div>
+
+        @for ($i = 1; $i <= 12; $i++)
+        
+        <div class="g g{{ $i }}-12 floatRight">
+            <p>Grid {{ $i }}</p>
         </div>
         <div class="cf"></div>
 
     @endfor
 
 </div>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br><br>
+<br>
+<br>
+<br>
+<br>
+<br>
