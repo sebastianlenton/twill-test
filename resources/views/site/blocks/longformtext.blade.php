66fc4b6c06f8799d047954bf75b10ebb203719f3
Sebastian Lenton <sebastian.lenton@gmail.com>
2018-11-21 18:10:41 +0000

    Longform text project block

--- resources/views/site/blocks/longformtext.blade.php
+++ resources/views/site/blocks/longformtext.blade.php
@@ -1,7 +1,16 @@
-<div style="background:yellow;margin-bottom:2rem;">
+<div class="cf"></div>
 
+<div class="g g2-12">
+    
     <h2>{{ $block->content['title'] }}</h2>
 
-    {!! $block->content['copy'] !!}
-    
 </div>
+
+<div class="g g6-12 gp2">
+    
+    {!! $block->content['copy'] !!}
+
+</div>    
+
+    
+    
